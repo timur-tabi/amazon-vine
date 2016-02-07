3200120b27e554ce731d20035492e9c39c7e47fd
Timur Tabi <timur@tabi.org>
2016-02-07 11:09:32 -0600

    Add support for Death By Captcha automated image captcha solving
    Death By Captcha is one of many paid online services that provides
    automated image captcha solving.  Since Amazon only seldomly prompts
    with an image captcha, it's handy to use an automated service.  Each
    image only costs about two cents.

--- README.md
+++ README.md
@@ -10,3 +10,46 @@ http://en.wikipedia.org/wiki/Amazon_Vine
 Amazon Vine is an invitation-only program, and there nothing you can do
 to get an invitation, not even from other Vine members.  I have no idea
 why I was invited.
+
+Death By Captcha support
+------------------------
+
+Death By Captcha is one of many paid online services that provides automated
+image captcha solving.  That is, you upload an captcha image to their
+web site, and few seconds later, it tells you what the image says.  Each
+image typically costs a couple cents to process.
+
+Sometimes Amazon gets suspicious and thinks that the script is a bot.
+It interrupts the login process and displays an image captcha.  Normally,
+the script will display the image on the screen and prompt the user to
+"solve" the captcha and type in what it says.
+
+Alternatively, the user can subscribe to the Death By Captcha service, and
+the script will use that service to automatically solve the captcha,
+without user intervention.  This service is a good option for users of
+this script because Amazon typically displays an image captcha only a few
+times a day (at most), and each image only costs about two cents to process.
+A basic Death By Captcha package costs about $7 for 5,000 images.  That is
+enough to last for years.
+
+1. Go to http://www.deathbycaptcha.com and create an account
+2. Sign up for the cheapest package ($6.95 for 5,000 views as of this
+   writing).  If you're unsure, you might be able to get a free trial.
+   Be sure to ask for access to the API.  Only paying customers get
+   access to the API.
+3. Download and unzip the Python package.  In it will be a file called
+   deathbycaptcha.py.  Put that file in the same place as amazon-vine.py.
+4. The script needs to know your Death By Captcha userid and password.
+   You can either set them on the command line with the --dbcu and --dbcp
+   parameters, or set the DEATHBYCAPTCHA_USERID and DEATHBYCAPTCHA_PASSWORD
+   environment variables.
+5. Start the script.  If/when the Amazon web site prompts an image captcha,
+   the script should use the Death By Captcha service transparently.
+
+NOTE: I make no guarantees that this script will work at all with the
+Death By Captcha service.  Anything could change at any time, and you
+assume all responsibility.  If the script goes haywire, it might
+repeatedly use the Death By Captcha service and use up all your funds.
+Or the service could change, and the only way to make it work is to
+modify this script, and I don't guarantee that I will continue to support
+Death By Captcha under any circumstances.
--- amazon-vine.py
+++ amazon-vine.py
@@ -52,12 +52,11 @@ except Exception as e:
     print e
     sys.exit(1)
 
-# Initialize the fake_useragent module.  This will take a minute
-print "Initializing fake_useragent"
-ua = fake_useragent.UserAgent(cache=False)
-
 your_queue_url = 'https://www.amazon.com/gp/vine/newsletter?ie=UTF8&tab=US_Default'
 vine_for_all_url = 'https://www.amazon.com/gp/vine/newsletter?ie=UTF8&tab=US_LastChance'
+
+# True = use Death By Captcha service
+use_deathbycaptcha = False
 
 # Show an image on the screen.  If the Python Image Library is available,
 # then use it.  Otherwise, open a web browser window.  I'm not sure if
@@ -71,6 +70,36 @@ def show_captcha(filename):
     except Exception as e:
         # No image library, use existing webbrowser
         webbrowser.open_new('file://' + filename)
+
+# Solve the image captcha.  If the Death By Captcha module is available,
+# then use it, otherwise display the image on the screen and prompt the
+# user to type in what it says.
+def solve_captcha(filename):
+    global options
+    global use_deathbycaptcha
+
+    show_captcha(filename)  # For debugging
+
+    if use_deathbycaptcha:
+        try:
+            dbc = deathbycaptcha.SocketClient(options.dbcu, options.dbcp)
+            captcha = dbc.decode(filename, 60)
+            if captcha:
+                print 'Death By Captcha returns %s' % captcha['text']
+                try:
+                    # It's not important if we can't get the balance
+                    print 'Death By Captcha balance: %.3f cents' % dbc.get_balance()
+                except Exception:
+                    pass
+                return captcha['text']
+            else:
+                print 'Death By Captcha could not solve captcha'
+        except Exception:
+            print e  # For debugging
+            pass
+
+    # If Death By Captcha is unavailable or fails, then fall back to manual
+    return raw_input('What word is in the image? ')
 
 def login():
     global options
@@ -111,8 +140,7 @@ def login():
         if captcha:
             response = br.retrieve(captcha['src'])
             print 'Login captcha detected, saved to', response[0]
-            show_captcha(os.path.realpath(response[0]))
-            value = raw_input('What word is in the image? ')
+            value = solve_captcha(os.path.realpath(response[0]))
             br.select_form(name='signIn')
             br['email'] = options.email
             br['password'] = options.password
@@ -279,6 +307,12 @@ parser.add_option("-p", dest="password",
 parser.add_option("-w", dest="wait",
     help="Number of minutes to wait between iterations (default is %default minutes)",
     type="int", default=10)
+parser.add_option("--dbcu", dest="dbcu",
+    help="Death By Captcha userid (default is DEATHBYCAPTCHA_USERID environment variable)",
+    type="string", default=os.getenv('DEATHBYCAPTCHA_USERID'))
+parser.add_option("--dbcp", dest="dbcp",
+    help="Death By Captcha password (default is DEATHBYCAPTCHA_PASSWORD environment variable)",
+    type="string", default=os.getenv('DEATHBYCAPTCHA_PASSWORD'))
 
 (options, args) = parser.parse_args()
 
@@ -294,6 +328,23 @@ if not options.password:
 
 # Test if asleep() works before we start
 asleep()
+
+# Initialize the fake_useragent module.  This will take a minute
+print "Initializing fake_useragent"
+ua = fake_useragent.UserAgent(cache=False)
+
+# Test for Death By Captcha
+try:
+    import deathbycaptcha
+    dbc = deathbycaptcha.SocketClient(options.dbcu, options.dbcp)
+    if not dbc:
+        print 'Death By Captcha login failed'
+    else:
+        print 'Death By Captcha balance: %.3f cents' % dbc.get_balance()
+        dbc.close()
+        use_deathbycaptcha = True
+except Exception:
+    pass
 
 br = login()
 if not br:
